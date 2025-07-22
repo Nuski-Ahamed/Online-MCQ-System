0944d8e018ad536f30ad6a340be9d62c803c47bd
Nuski Ahamed <nuskiahamed111@gmail.com>
2025-07-22 23:05:53 +0530

    base project setup with Angular, Node.js,MongoDB and TailwindCSS

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+// Connect MongoDB
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+
+// Routes
+const examRoutes = require('./routes/exams');
+app.use('/api/exams', examRoutes);
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
